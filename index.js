b3868c0579f1bdb12032a659031d78a757e79927
Archibald Macharia Gacheru <archibald.gacheru@student.moringaschool.com>
2023-07-17 22:32:47 +0300

    function trials

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Follow along with the examples here
+function sayHello(hello){
+    hello="Hello World"
+    return hello;
+}
+//sayHello("Hello World");
+
+function add(x, y){
+    sum = x + y;
+    return sum;;
+}
+//console.log(add(3,5));
+
+let teacher = `The teacher has taught me ${sayHello()} and how to add 3 + 5 which is ${add(3,5)}`
+console.log(teacher);
